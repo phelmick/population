397ca5c93827b5ef5e1dfdf78b1eb76c0a07a3cb
Paul Helmick <phelmick@gmail.com>
2013-08-16 21:50:14 -0400

    Step 6

---
+++ lib/area.rb
@@ -0,0 +1,12 @@
+class Area
+  def initialize(hash)
+    @zipcode = hash[:zipcode].to_i || 0
+    @estimated_population = hash[:estimated_population].to_i || 0
+    @city = hash[:city] || "n/a"
+    @state = hash[:state] || "n/a"
+  end
+
+  def to_s
+    "#{city}, #{state} #{zipcode} has #{estimated_population} people."
+  end
+end
