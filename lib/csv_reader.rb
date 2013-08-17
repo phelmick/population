8bf50e54d911fe52ef7c7e4eab3a9fa83225dba9
Paul Helmick <phelmick@gmail.com>
2013-08-16 21:35:05 -0400

    Step 5

---
+++ lib/csv_reader.rb
@@ -0,0 +1,44 @@
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
+
+class CSVReader
+
+  attr_accessor :fname, :headers
+
+  def initialize(filename)
+    @fname = filename
+  end
+
+  def headers=(header_str)
+    @headers = header_str.split(',')
+    @headers.map! do |h|
+
+      # remove quotes
+      h.gsub!('"', '')
+
+      # remove new line
+      h.strip!
+
+      # convert to a symbol
+      h.underscore.to_sym
+    end
+  end
+  
+    def create_hash(values)
+    h = {}
+    @headers.each_with_index do |header, i|
+      # remove new lines from the value
+      value = values[i].strip.gsub('"', '')
+      h[header] = value unless value.empty?
+    end
+    h
+  end
+end
